0f0cc133133b24f2d712511bc12132c770a2cb20
sparKlZJuNiO <jrrossjob@gmail.com>
2024-10-23 12:46:53 +0100

    NICE

---
+++ Assets/Scripts/WaypointFollower.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollower : MonoBehaviour
+{
+    [SerializeField]
+    private void Update()
+    {
+        
+    }
+}
